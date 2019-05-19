9f119719b0ea9d15a5595782b1906a16d22ba458
Bracken Mosbacker <bmosbacker@imsglobal.org>
2019-05-19 15:52:08 -0700

    add a little description and styling

--- src/components/SendEvents/Profiles.js
+++ src/components/SendEvents/Profiles.js
@@ -29,7 +29,8 @@ export class Profiles extends Component {
     render() {
         return <div>
             <h2>Profiles</h2>
-
+            <p className="description">Caliper Metric Profiles (<a href="https://www.imsglobal.org/sites/default/files/caliper/v1p1/caliper-spec-v1p1/caliper-spec-v1p1.html#profiles" target="_blank">documentation</a>) provide a common way to describe user interactions using a shared vocabulary. For example: reading a document, taking a quiz, or watching a video.
+            </p>
             {this.state.profiles.map((profile, index) => {
                     return <Profile profile={profile} key={index} />
                 })}
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,7 +6,15 @@ const Welcome = (props) => {
     return (
             <div>
                 <Header page={props.page}/>
-                <p>Hello, eh!</p>
+                <p className="description">This is an example application to help you learn a little bit
+                    about the IMS Caliper Analytics standard.</p>
+
+                <p className="description">On the "Send Events" page you can explore some Caliper
+                    Profiles and events and even send them to this example
+                    server or your own.</p>
+
+                <p className="description">On the "Receive Events" page you can see all events sent from
+                    this app or your own.</p>
             </div>
     );
 };
